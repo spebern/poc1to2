6df332fb2b9a0d1d0fb4dd6b27ea53ed8d9aea3e
Bernhard Specht <bernhard@specht.net>
2018-06-02 14:46:27 +0200

    working inline version

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,185 @@
-#[macro_use]
 extern crate clap;
-extern crate systemstat;
-
-use systemstat::{System, Platform};
+use clap::{Arg, App};
+use std::io::prelude::*;
+use std::path::Path;
+use std::fs;
 use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::SeekFrom;
 
-struct PlotFile {
-    account_id: u64,
-    start_nonce: u64,
-    nonce_count: u64,
+const SCOOPS_IN_NONCE: i64 = 4096;
+const SHABAL256_HASH_SIZE: i64 = 32;
+const SCOOP_SIZE: i64 = SHABAL256_HASH_SIZE * 2;
+const NONCE_SIZE: i64 = SCOOP_SIZE * SCOOPS_IN_NONCE;
+
+struct Plot<'a> {
+    id: u64,
+    offset: u64,
+    nonces: i64,
     stagger: u64,
-    optimized: bool,
+    path: &'a Path
 }
 
-fn parse_file_name(file_name: String) -> Option<PlotFile> {
-    let splits: Vec<_> = file_name.split("_").collect();
-    if splits.len() < 4 {
-        println!("plot has invalid file name: {}", file_name);
-        return None;
-    }
+impl<'a> Plot<'a> {
+    fn new(path: &str) -> Plot {
+        let parts: Vec<&str> = path.split("_").collect();
+        if parts.len() < 4 {
+            panic!("plot file has wrong format")
+        }
 
-    let account_id = splits[0].parse::<u64>().ok();
-    if account_id == None {
-        println!("failed to parse account id");
-        return None;
-    }
+        let id_res = parts[0].parse::<u64>();
+        if id_res.is_err() {
+            panic!("id of plotfile has wrong format")
+        }
 
-    let start_nonce = splits[1].parse::<u64>().ok();
-    if start_nonce == None {
-        println!("failed to parse start nonce");
-        return None;
-    }
+        let offset_res = parts[1].parse::<u64>();
+        if offset_res.is_err() {
+            panic!("offset of plotfile has wrong format")
+        }
 
-    let nonce_count = splits[2].parse::<u64>().ok();
-    if nonce_count == None {
-        println!("failed to parse nonce count");
-        return None;
-    }
+        let nonces_res = parts[2].parse::<i64>();
+        if nonces_res.is_err() {
+            panic!("nonces of plotfile has wrong format")
+        }
 
-    let stagger = splits[3].parse::<u64>().ok();
-    if stagger == None {
-        println!("failed to parse stagger");
-        return None;
-    }
+        let stagger_res = parts[3].parse::<u64>();
+        if stagger_res.is_err() {
+            panic!("stagger of plotfile has wrong format")
+        }
 
-    return Some(PlotFile{
-        account_id: account_id.unwrap(),
-        start_nonce: start_nonce.unwrap(),
-        nonce_count: nonce_count.unwrap(),
-        stagger: stagger.unwrap(),
-        optimized: nonce_count == stagger,
-    });
-}
+        let path = Path::new(path);
+        if !path.exists() {
+            panic!("plot path does not exists");
+        };
+        if !path.is_file() {
+            panic!("plot path is not a file");
+        };
 
-fn main() {
+        let nonces = nonces_res.unwrap();
+        let size = fs::metadata(path).unwrap().len();
+        let exp_size = nonces * NONCE_SIZE;
+        if size != exp_size as u64 {
+            panic!("expected plot size {} but got {}", exp_size, size);
+        };
 
-    // COMMAND LINE PROCESSING
-
-    let matches = clap_app!(myapp =>
-        (version: "0.0.1")
-        (author: "PoC Consortium <bots@cryptoguru.org>")
-        (about: "Burst PoC1 to PoC2 converter")
-        (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
-        (@arg INPUT: +required "Sets the input file to use")
-        (@arg debug: -d ... "Sets the level of debugging information")
-        (@subcommand test =>
-            (about: "controls testing features")
-            (version: "1.3")
-            (author: "Someone E. <someone_else@other.com>")
-            (@arg verbose: -v --verbose "Print test information verbosely")
-        )
-    ).get_matches();
-   // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
-
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    let input = matches.value_of("INPUT").unwrap();
-    println!("Using input file: {}", input);
-
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
-
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Printing normally...");
+        Plot{
+            id: id_res.unwrap(),
+            offset: offset_res.unwrap(),
+            nonces: nonces,
+            stagger: stagger_res.unwrap(),
+            path: path
         }
     }
 
-    // FREE SYSTEM MEMORY
-    
-    let sys = System::new();
+    fn convert(&self) {
+        let mut from = File::open(self.path).unwrap();
+        let mut to = OpenOptions::new().write(true).open(self.path).unwrap();
+        let block_size = self.nonces * SCOOP_SIZE;
+        let mut buffer1 = vec![0; block_size as usize];
+        let mut buffer2 = vec![0; block_size as usize];
 
-    match sys.memory() {
-        Ok(mem) => println!("\nMemory: {} used / {} ({} bytes) total ({:?})", mem.total - mem.free, mem.total, mem.total.as_usize(), mem.platform_memory),
-        Err(x) => println!("\nMemory: error: {}", x)
+        for scoop in 0i64 .. SCOOPS_IN_NONCE / 2 {
+            let pos = scoop * block_size;
+
+            from.seek(SeekFrom::Start(pos as u64));
+            let numread = from.read(&mut buffer1).unwrap();
+            if numread as i64 != block_size {
+                panic!("read {} bytes instead of {}", numread, block_size);
+            }
+
+            from.seek(SeekFrom::End(-pos - block_size));
+            let numread = from.read(&mut buffer2).unwrap();
+            if numread as i64 != block_size {
+                panic!("read {} bytes instead of {}", numread, block_size);
+            }
+
+            let mut off: usize = 32;
+            for _ in 0 .. self.nonces {
+                let mut hash1 = [0;SHABAL256_HASH_SIZE as usize];
+                hash1.copy_from_slice(&buffer1[off..off+SHABAL256_HASH_SIZE as usize]);
+                buffer1[off..off+SHABAL256_HASH_SIZE as usize].copy_from_slice(
+                    &buffer2[off..off+SHABAL256_HASH_SIZE as usize]);
+                buffer2[off..off+SHABAL256_HASH_SIZE as usize].copy_from_slice(&hash1);
+                off += SCOOP_SIZE as usize;
+            }
+
+            to.seek(SeekFrom::End(-pos - block_size));
+            let numwrite = to.write(&buffer2).unwrap();
+            if numwrite as i64 != block_size {
+                panic!("wrote {} bytes instead of {}", numread, block_size)
+            }
+
+            to.seek(SeekFrom::Start(pos as u64));
+            let numwrite = to.write(&buffer1).unwrap();
+            if numwrite as i64 != block_size {
+                panic!("wrote {} bytes instead of {}", numread, block_size)
+            }
+        }
+
+        fs::rename(self.path, self.poc2_name());
     }
-    
-    // READ FILE TO MEMORY
 
-    let f = File::open(input).expect("file not found");
-    
+    fn poc2_name(&self) -> String {
+        self.id.to_string() + "_" + &self.offset.to_string() + "_" + &self.nonces.to_string()
+    }
+}
+
+fn main() {
+    let matches = App::new("PoC1 to PoC2 Converter")
+        .version("0.0.1")
+        .author("PoC Consortium <bots@cryptoguru.org>")
+        .about("converts PoC1 plots to PoC2 plots")
+        .arg(Arg::with_name("in")
+             .required(true)
+             .index(1))
+        .arg(Arg::with_name("out")
+             .short("o")
+             .long("out")
+             .help("Define a directory to write the converted plot file to. This switches
+to copy on write mode. (Else in-place is default) and allows you to
+fasten up the conversion at the expense of temporary additional HDD
+space.")
+             .takes_value(true)
+             .value_name("OUT")).get_matches();
+
+    let plot = Plot::new(matches.value_of("in").unwrap());
+    plot.convert();
+}
+
+fn preallocate(path: &str, size: u64) -> std::io::Result<()>{
+    let f = File::create(path)?;
+    f.set_len(size)?;
+    Ok(())
 }
 
 #[cfg(test)]
-mod test;
+mod tests {
+    extern crate md5;
+    use super::*;
+
+    #[test]
+    fn test_plot() {
+        let plot_file = "11253871103436815155_0_10_10";
+        fs::copy(plot_file.to_owned() + ".orig", plot_file);
+
+        let plot = Plot::new(plot_file);
+
+        assert_eq!(plot.id, 11253871103436815155);
+        assert_eq!(plot.offset, 0);
+        assert_eq!(plot.nonces, 10);
+        assert_eq!(plot.stagger, 10);
+        assert_eq!(plot.path, Path::new(plot_file));
+
+        let poc2_plot_file = plot.poc2_name();
+        assert_eq!(poc2_plot_file, "11253871103436815155_0_10");
+
+        plot.convert();
+        let mut buffer = Vec::new();
+        File::open(&poc2_plot_file).unwrap().read_to_end(&mut buffer);
+
+        let digest = md5::compute(buffer);
+        assert_eq!(format!("{:x}", digest), "5dbd4aa4033b5877f37744a12c0f573c");
+
+        fs::remove_file(&poc2_plot_file);
+    }
+}
